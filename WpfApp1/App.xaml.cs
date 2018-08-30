ffa68ff3e32bb32b05dedb14999abb17044e9ab8
黄冬 <huangdong@youkia.net>
2018-08-30 10:30:35 +0800

    fix WPF Clipboard exception

--- WpfApp1/App.xaml.cs
+++ WpfApp1/App.xaml.cs
@@ -11,5 +11,15 @@ namespace WpfApp1 {
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application {
+        private void Application_DispatcherUnhandledException ( object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e ) {
+            // fix WPF Clipboard exception
+            // https://stackoverflow.com/questions/12769264/openclipboard-failed-when-copy-pasting-data-from-wpf-datagrid
+            var comException = e.Exception as System.Runtime.InteropServices.COMException;
+            if (comException != null && comException.ErrorCode == -2147221040)
+                e.Handled = true;
+            else
+                MessageBox.Show (e.Exception.Message);
+            e.Handled = true;
+        }
     }
 }
